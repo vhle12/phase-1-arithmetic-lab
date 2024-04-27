1cf88f89025c5658a42e56ef5a122debb531b8c4
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-04-27 17:44:17 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() *100)
+
+const num3 = 24
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(1,2,3,4,20)
